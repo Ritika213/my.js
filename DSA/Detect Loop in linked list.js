f02c9c3b5896cc4ec129d7127706626ba5a1bade
Ritika213 <ritikakgupta6@gmail.com>
2024-01-05 12:08:12 +0530

    detect loop in linked list

---
+++ DSA/Detect Loop in linked list.js
@@ -0,0 +1,34 @@
+//Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.
+
+
+Input:
+N = 3
+value = //{1,3,4}
+//x(position at which tail is connected) = 2
+Output// True
+Explanation //In above test case N = 3.
+//The linked list with nodes N = 3 is given. Then value of x=2 is given which means last node is connected with xth node of linked list. Therefore, there
+//exists a loop.
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    detectLoop(head)
+    {
+        
+         let count=0; 
+        if(head==null)
+        {
+            return false;
+        }
+        while(head!=null)
+        {
+            head=head.next;
+            count++;
+            if(count>100000)
+             return true;
+        }
+        return false;
+        
+    }
+    
+}

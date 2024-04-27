e272f633b931a2bf73579b7c37039a4a078b0793
Ritika213 <ritikakgupta6@gmail.com>
2024-04-27 12:07:13 +0530

    subarray with 0 sum

---
+++ DSA/Subarray with 0 sum.js
@@ -0,0 +1,62 @@
+//Given an array of integers. Find if there is a subarray (of size at-least one) with 0 sum. You just need to return true/false
+// depending upon whether there is a subarray present with 0-sum or not. Printing will be taken care by the driver code.
+
+//Example 1:
+
+Input:
+n = 5
+arr = //{4,2,-3,1,6}
+Output 
+Yes
+Explanation 
+//2, -3, 1 is the subarray with sum 0.
+
+//Example 2:
+
+Input:
+n = 5
+arr =// {4,2,0,1,6}
+Output
+Yes
+Explanation: 
+//0 is one of the element in the array so there exist a subarray with sum 0.
+ Task:
+//You only need to complete the function subArrayExists() that takes array and n as parameters and returns true or false.
+
+//Expected Time Complexity: O(n).
+//Expected Auxiliary Space: O(n).
+
+Constraints:
+1 <= n <= 104
+-105 <= a[i] <= 105
+
+class Solution {
+    //Function to check whether there is a subarray present with 0-sum or not.
+    subArrayExists(arr, n){
+        
+        let mp = new Map();
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        sum += arr[i];
+        if (mp.has(sum)) {
+            mp.set(sum, mp.get(sum) + 1);
+        } else {
+            mp.set(sum, 1);
+        }
+    }
+
+    if (mp.has(0)) {
+        return true;
+    }
+
+    for (let val of mp.entries()) {
+        if (val[1] > 1) {
+            return true;
+        }
+    }
+
+    return false;
+    }
+    
+}

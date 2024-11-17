d34da936b47afae47c4d82153745fe6634b4619c
Ritika213 <ritikakgupta6@gmail.com>
2024-11-17 12:38:01 +0530

    reverse an array

---
+++ DSA/Reverse an Array.js
@@ -0,0 +1,36 @@
+//given an array of integers arr[]. Your task is to reverse the given array.
+
+Examples:
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: //The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, 
+//the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+Input: arr = [4, 5, 2]
+Output: [2, 5, 4]
+Explanation:// The elements of the array are 4 5 2. The reversed array will be 2 5 4.
+Input: arr = [1]
+Output: [1]
+Explanation// The array has only single element, hence the reversed array is same as the original.
+
+Constraints:
+1<=arr.size()<=105
+0<=arr[i]<=105
+
+class Solution {
+    // Function to reverse the array.
+    reverseArray(arr) {
+        
+         let i = 0;
+        let j = arr.length - 1;
+        while (i < j) {
+            // Swap arr[i] and arr[j]
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        
+    }
+}

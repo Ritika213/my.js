84a329f5b9be0d7e2fa6f24fe871bac0bd5a18d3
Ritika213 <ritikakgupta6@gmail.com>
2025-03-04 13:07:19 +0530

    longest increasing subsequence

---
+++ DSA/Longest Increasing Subsequence.js
@@ -0,0 +1,39 @@
+//Given an array arr[] of non-negative integers, the task is to find the length of the Longest Strictly Increasing Subsequence (LIS).
+
+//A subsequence is strictly increasing if each element in the subsequence is strictly less than the next element.
+
+Examples:
+
+Input: arr = [5, 8, 3, 7, 9, 1]
+Output: 3
+Explanation:// The longest strictly increasing subsequence could be [5, 7, 9], which has a length of 3.
+Input: arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+Output: 6
+Explanation// One of the possible longest strictly increasing subsequences is [0, 2, 6, 9, 13, 15], which has a length of 6.
+
+Input: arr = [3, 10, 2, 1, 20]
+Output: 3
+Explanation:// The longest strictly increasing subsequence could be [3, 10, 20], which has a length of 3.
+Constraints
+//1 ≤ arr.size() ≤ 103
+//0 ≤ arr[i] ≤ 106
+
+class Solution {
+    lis(arr) {
+        
+         let n = arr.length;
+        let maxi = new Array(n).fill(1); // Use an array instead of vector<int>
+        let ans = 1;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    maxi[i] = Math.max(maxi[i], maxi[j] + 1);
+                }
+            }
+            ans = Math.max(ans, maxi[i]);
+        }
+
+        return ans;
+    }
+}

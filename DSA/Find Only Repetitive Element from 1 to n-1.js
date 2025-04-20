9d3181a37b59b780b9d7af7810c01c029794ad83
Ritika213 <ritikakgupta6@gmail.com>
2025-04-20 12:26:22 +0530

    find only repetitive element from 1 to n-1

---
+++ DSA/Find Only Repetitive Element from 1 to n-1.js
@@ -0,0 +1,35 @@
+//Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your 
+//task is to find the repetitive element.
+
+Note: //It is guaranteed that there is a repeating element present in the array.
+
+Examples:
+
+Input: arr = [1, 3, 2, 3, 4]
+Output: 3 
+Explanation:// The number 3 is the only repeating element.
+Input: arr = [1, 5, 1, 2, 3, 4]
+Output: 1  
+Explanation: //The number 1 is the only repeating element.
+Input: arr = [1, 1]  
+Output: 1
+Explanation //The array is of size 2 with both elements being 1, making 1 the repeating element.
+
+Constraints
+//2 ≤ arr.size() ≤ 105
+//1 ≤ arr[i] ≤ n-1 
+
+class Solution {
+
+    findDuplicate(arr) {
+        
+        let ans = 0;
+        let n = arr.length;
+        for(let i=0; i<n-1;i++){
+            ans ^= (i+1)
+            ans ^= arr[i]
+        }
+        ans ^= arr[n-1];
+        return ans;
+    }
+}

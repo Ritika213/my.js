ef5e959ba73be06a9f2d78c14ee4d1457252d2a6
Ritika213 <ritikakgupta6@gmail.com>
2025-05-13 19:26:16 +0530

    nCr

---
+++ DSA/nCr.js
@@ -0,0 +1,43 @@
+//Given two integer values n and r, the task is to find the value of Binomial Coefficient nCr
+
+//A binomial coefficient nCr can be defined as the coefficient of xr in the expansion of (1 + x)n that gives the number of ways to 
+//choose r objects from a set of n objects without considering the order.
+//The binomial coefficient nCr is calculated as : C(n,r) = n! / r! * (n-r) !
+Note:// If r is greater than n, return 0.
+//It is guaranteed that the value of nCr will fit within a 32-bit integer.
+
+Examples
+
+Input: n = 5, r = 2
+Output: 10
+Explaination:// The value of 5C2 is calculated as 5!/(5−2)!*2! = 5!/3!*2! = 10.
+Input: n = 2, r = 4
+Output: 0
+Explaination:// Since r is greater than n, thus 2C4 = 0
+Input: n = 5, r = 0
+Output: 1
+Explaination: //The value of 5C0 is calculated as 5!/(5−0)!*0! = 5!/5!*0! = 1.
+Constraints
+//1 ≤ n ≤ 100
+//1 ≤ r ≤ 100
+
+
+const mod = 1e9 +7;
+
+
+class Solution {
+    nCr(n, r) {
+        
+       if (n < r) return 0;
+
+        let pascal = [];
+        for (let i = 0; i <= n; i++) {
+            let v = new Array(i + 1).fill(1);
+            for (let j = 1; j < i; j++) {
+                v[j] = (pascal[j] + pascal[j - 1]) % mod;
+            }
+            pascal = v;
+        }
+        return pascal[r]; 
+    }
+}

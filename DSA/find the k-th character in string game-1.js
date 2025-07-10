c14808489b7e0b60ee32bbc8896f66b8ff8f5b87
Ritika213 <ritikakgupta6@gmail.com>
2025-07-10 14:20:55 +0530

    find the k-th character in string game

---
+++ DSA/find the k-th character in string game-1.js
@@ -0,0 +1,49 @@
+//Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+
+//given a positive integer k.
+
+//Now Bob will ask Alice to perform the following operation forever:
+
+//Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+//For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+
+//Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+
+ 
+
+//Example 1:
+
+Input: k = 5
+
+Output: "b"
+
+Explanation
+
+//Initially, word = "a". We need to do the operation three times:
+
+//Generated string is "b", word becomes "ab".
+
+//Generated string is "bc", word becomes "abbc".
+//Generated string is "bccd", word becomes "abbcbccd".
+//Example 2:
+
+Input: k = 10
+
+Output: "c"
+
+ 
+
+Constraints:
+
+1 <= k <= 500
+
+
+var kthCharacter = function(k) {
+  const res = [0];
+  while(k > res.length){
+    for(let i=0,n=res.length;i<n;i++){
+        res.push(res[i] +1)
+    }
+  }
+  return String.fromCharCode(res[k-1] % 26 + 'a'.charCodeAt(0))
+};

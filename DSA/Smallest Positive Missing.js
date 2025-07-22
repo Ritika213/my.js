6107d76c490e242c884dcb6a5057ee5ea3c490be
Ritika213 <ritikakgupta6@gmail.com>
2025-07-22 13:59:38 +0530

    smallest positive missing

---
+++ DSA/Smallest Positive Missing.js
@@ -0,0 +1,38 @@
+//given an integer array arr[]. Your task is to find the smallest positive number missing from the array.
+
+Note: //Positive number starts from 1. The array can have negative integers too.
+
+Examples:
+
+Input: arr = [2, -3, 4, 1, 1, 7]
+Output: 3
+Explanation:// Smallest positive missing number is 3.
+Input: arr= [5, 3, 2, 5, 1]
+Output: 4
+Explanation:// Smallest positive missing number is 4.
+Input: arr = [-8, 0, -1, -4, -3]
+Output: 1
+Explanation:// Smallest positive missing number is 1.
+Constraints  
+//1 ≤ arr.size() ≤ 105
+//-106 ≤ arr[i] ≤ 106
+
+class Solution {
+    missingNumber(arr) {
+        
+       arr.sort((a,b)=>a-b);
+        let missing = 1;
+        for(let i = 0;i<arr.length;i++){
+            if(arr[i]<missing){
+                continue;
+            }
+            if(arr[i]===missing){
+                missing++;
+            }
+            else if(arr[i] > missing) {
+                return missing;
+            }
+        }
+        return missing; 
+    }
+}

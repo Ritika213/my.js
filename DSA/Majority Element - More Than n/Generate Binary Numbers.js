476c6325c4fdd30aa155d7936949b22fb081a367
Ritika213 <ritikakgupta6@gmail.com>
2025-09-25 11:33:01 +0530

    generate binary numbers

---
+++ DSA/Majority Element - More Than n/Generate Binary Numbers.js
@@ -0,0 +1,24 @@
+//Given a number n. The task is to generate all binary numbers with decimal values from 1 to n.
+
+Examples:
+
+Input: n = 4
+Output: ["1", "10", "11", "100"]
+Explanation: //Binary numbers from 1 to 4 are 1, 10, 11 and 100.
+Input: n = 6
+Output: ["1", "10", "11", "100", "101", "110"]
+Explanation:// Binary numbers from 1 to 6 are 1, 10, 11, 100, 101 and 110.
+Constraints
+//1 ≤ n ≤ 106
+
+class Solution {
+    generateBinary(n) {
+        
+         let  res = [];
+        for(let i=1;i<=n;i++){
+             res.push(i.toString(2)); 
+        }
+        
+        return res;
+    }
+}

80409f0c156da14c0719d01ca1c74f44a1386c1b
Ritika213 <ritikakgupta6@gmail.com>
2025-11-06 13:01:38 +0530

    ways to the a floor

---
+++ DSA/Majority Element - More Than n/Ways To Tile A Floor.js
@@ -0,0 +1,50 @@
+//Given a floor of dimensions 2 x n and tiles of dimensions 2 x 1, the task is to find the number of ways the floor can be tiled.
+// A tile can either be placed horizontally i.e as a 1 x 2 tile or vertically i.e as 2 x 1 tile. 
+
+Note: //Two tiling arrangements are considered different if the placement of at least one tile differs.
+
+Examples :
+
+Input: n = 3
+Output: 3
+Explanation //We need 3 tiles to tile the board of size 2 x 3.
+//We can tile in following ways:
+//1) Place all 3 tiles vertically.
+//2) Place first tile vertically and remaining 2 tiles horizontally.
+//3) Place first 2 tiles horizontally and remaining tiles vertically.
+
+Input: n = 4
+Output: 5
+Explanation:// We need 4 tiles to tile the board of size 2 x 4.
+//We can tile in following ways:
+//1) All 4 vertical
+//2) All 4 horizontal
+//3) First 2 vertical, remaining 2 horizontal.
+//4) First 2 horizontal, remaining 2 vertical.
+//5) Corner 2 vertical, middle 2 horizontal.
+
+Constraints
+//1 ≤ n ≤ 45
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+class Solution {
+    numberOfWays(n) {
+        
+       if(n===1) return 1;
+       let i=1;
+       let j=2;
+       while(n >2){
+           let curr = i+j;
+           i=j;
+           j= curr;
+           n--;
+       }
+       return j;
+       return 0;
+    }
+}

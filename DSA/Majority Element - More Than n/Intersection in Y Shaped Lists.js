68b698c12bcc3062d01e42b4d29c52a35c4593a2
Ritika213 <ritikakgupta6@gmail.com>
2026-01-01 13:15:27 +0530

    intersection in y shaped lists

---
+++ DSA/Majority Element - More Than n/Intersection in Y Shaped Lists.js
@@ -0,0 +1,48 @@
+//given the heads of two non-empty singly linked lists, head1 and head2, that intersect at a certain point. Return that Node where
+//  these two linked lists intersect.
+
+Note:// It is guaranteed that the intersected node always exists.
+
+//In the custom input you have to give input for CommonList which pointed at the end of both head1 and head2 to form a Y-shaped linked list.
+Examples:
+
+Input: head1:// 10 -> 15 -> 30, head2: 3 -> 6 -> 9 -> 15 -> 30
+Output: 15
+Explanation// From the above image, it is clearly seen that the common part is 15 -> 30, whose starting point is 15.
+Input: head1:// 4 -> 1 -> 8 -> 5, head2: 5 -> 6 -> 1 -> 8 -> 5
+Output: 1
+Explanation:// From the above image, it is clearly seen that the common part is 1 -> 8 -> 5, whose starting point is 1.
+    
+Constraints
+//2 ≤ total number of nodes ≤ 2*105
+//-104 ≤ node->data ≤ 104
+
+/**
+ * @param {Node} head1
+ * @param {Node} head2
+ * @returns {Node}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    intersectPoint(head1, head2) {
+        
+         let curr1=head1;
+        let curr2=head2;
+        while(curr1!=curr2){
+        curr1= curr1 === null ? head2 : curr1.next;
+        curr2 = curr2 === null ? head1 : curr2.next;
+        }
+        return curr1;
+    }
+}
+
+
+    

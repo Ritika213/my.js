99ac0a3ee301d95850b9aab3b0d18d17a8458d66
Ritika213 <ritikakgupta6@gmail.com>
2026-01-02 13:42:07 +0530

    sort 0s,1s and 2s

---
+++ DSA/Majority Element - More Than n/Sort 0s, 1s and 2s.js
@@ -0,0 +1,45 @@
+//Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+Note: //You need to solve this problem without utilizing the built-in sort function.
+
+Examples:
+
+Input: arr = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation:// 0s, 1s and 2s are segregated into ascending order.
+Input: arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+Explanation:// 0s, 1s and 2s are segregated into ascending order.
+//Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+Constraints
+//1 ≤ arr.size() ≤ 106
+//0 ≤ arr[i] ≤ 2
+
+/**
+ * @param {number[]} arr
+ * @returns {void}
+ */
+
+class Solution {
+    sort012(arr) {
+        
+         let low = 0, mid = 0, high = arr.length - 1;
+
+      while (mid <= high) {
+    switch (arr[mid]) {
+        case 0:
+            [arr[low], arr[mid]] = [arr[mid], arr[low]]; // Swap arr[low] and arr[mid]
+            low++;
+            mid++;
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            [arr[mid], arr[high]] = [arr[high], arr[mid]]; // Swap arr[mid] and arr[high]
+            high--;
+            break;
+    }
+      }
+    }
+}

81c887f7b4d0a6c2eff00433a1127cd3eb61292e
Ritika213 <ritikakgupta6@gmail.com>
2026-02-05 13:28:54 +0530

    maximize number of 1's

---
+++ Maximize Number of 1's.js
@@ -0,0 +1,42 @@
+//Given a binary array arr[] containing only 0s and 1s and an integer k, you are allowed to flip at most k 0s to 1s. 
+// Find the maximum number of consecutive 1's that can be obtained in the array after performing the operation at most k times.
+
+Examples:
+
+Input: arr = [1, 0, 1], k = 1
+Output: 3
+Explanation:// By flipping the zero at index 1, we get the longest subarray from index 0 to 2 containing all 1’s.
+Input: arr = [1, 0, 0, 1, 0, 1, 0, 1], k = 2
+Output: 5
+Explanation:// By flipping the zeroes at indices 4 and 6, we get the longest subarray from index 3 to 7 containing all 1’s.
+Input: arr = [1, 1], k = 2
+Output: 2
+Explanation //Since the array is already having the max consecutive 1's, hence we dont need to perform any operation. Hence the answer is 2.
+Constraints
+//1 ≤ arr.size() ≤ 105
+//0 ≤ k ≤ arr.size()
+//0 ≤ arr[i] ≤ 1
+
+class Solution {
+    maxOnes(arr, k) {
+        
+         let ans=0;
+        let l=0;
+        let h=0;
+        let zero=0;
+        while(h<arr.length){
+            if(arr[h]===0){
+                zero++;
+            }
+            while(l<=h && zero>k){
+                if(arr[l]===0){
+                    zero--;
+                }
+                l++;
+            }
+            ans=Math.max(ans,h-l+1);
+            h++;
+        }
+        return ans;
+    }
+}

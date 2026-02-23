f7bfffc1596ccd0dede9b5a796066203363d1b4c
Ritika213 <ritikakgupta6@gmail.com>
2026-02-23 12:38:57 +0530

    union of arrays with duplicates

---
+++ Union of Arrays with Duplicates.js
@@ -0,0 +1,59 @@
+//given two arrays a[] and b[], return the Union of both the arrays in any order.
+
+//The Union of two arrays is a collection of all distinct elements present in either of the arrays. If an element appears more than
+//  once in one or both arrays, it should be included only once in the result.
+
+Note:// Elements of a[] and b[] are not necessarily distinct.
+//Note that, You can return the Union in any order but the driver code will print the result in sorted order only.
+
+Examples:
+
+Input: a = [1, 2, 3, 2, 1], b = [3, 2, 2, 3, 3, 2]
+Output: [1, 2, 3]
+Explanation:// Union set of both the arrays will be 1, 2 and 3.
+Input: a = [1, 2, 3], b = [4, 5, 6] 
+Output: [1, 2, 3, 4, 5, 6]
+Explanation:// Union set of both the arrays will be 1, 2, 3, 4, 5 and 6.
+Input: a = [1, 2, 1, 1, 2], b = [2, 2, 1, 2, 1] 
+Output: [1, 2]
+Explanation //Union set of both the arrays will be 1 and 2.
+
+Constraints
+//1 ≤ a.size(), b.size() ≤ 106
+//0 ≤ a[i], b[i] ≤ 105
+
+/**
+ * @param {number[]} a
+ * @param {number[]} b
+ * @returns {number[]}
+ */
+class Solution {
+    findUnion(a, b) {
+        
+         // Create visited array
+        let vis = new Array(100100).fill(0);
+        let maxc = -1;
+
+        // Mark elements of array a
+        for (let e of a) {
+            vis[e] = 1;
+            maxc = Math.max(maxc, e);
+        }
+
+        // Mark elements of array b
+        for (let e of b) {
+            vis[e] = 1;
+            maxc = Math.max(maxc, e);
+        }
+
+        // Collect union elements
+        let ans = [];
+        for (let i = 0; i <= maxc; i++) {
+            if (vis[i] === 1) {
+                ans.push(i);
+            }
+        }
+
+        return ans;
+    }
+}

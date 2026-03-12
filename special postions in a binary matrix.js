d3c4b2718840e4c0039166a011812181b28554ac
Ritika213 <ritikakgupta6@gmail.com>
2026-03-12 18:54:51 +0530

    special postions in a binary matrix

---
+++ special postions in a binary matrix.js
@@ -0,0 +1,57 @@
+//Given an m x n binary matrix mat, return the number of special positions in mat.
+
+//A position (i, j) is called special if mat[i][j] == 1 and all other elements in row i and column j are 0 (rows and columns are 0-indexed).
+
+ 
+
+//Example 1:
+
+
+Input: mat = [[1,0,0],[0,0,1],[1,0,0]]
+Output: 1
+Explanation// (1, 2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
+
+Input: mat = [[1,0,0],[0,0,1],[1,0,0]]
+Output: 1
+Explanation:// (1, 2) is a special position because mat[1][2] == 1 and all other elements in row 1 and column 2 are 0.
+//Example 2:
+
+
+Input: mat = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+Explanation //(0, 0), (1, 1) and (2, 2) are special positions.
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 100
+//mat[i][j] is either 0 or 1.
+
+
+var numSpecial = function(mat) {
+    var count = 0
+    var rl = mat.length
+
+    for(var i = 0; i< rl; i++){
+        var rowsum = mat[i].reduce(function(a,b){return a+b})
+
+        if(rowsum == 1){
+            var index = mat[i].indexOf(1)
+            
+            var colsum = 0;
+            for (var j = 0; j < rl; j++){
+                colsum = colsum + mat[j][index]
+            }
+            
+            if (colsum == 1){
+                count++
+            }
+        }
+    }
+
+    return count
+
+};   
+
+ 

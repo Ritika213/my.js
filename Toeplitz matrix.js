3a1ce54a1a407bafd92db26746831bfcf922967c
Ritika213 <ritikakgupta6@gmail.com>
2026-04-12 13:15:53 +0530

    toepiltz matrix

---
+++ Toeplitz matrix.js
@@ -0,0 +1,54 @@
+//A Toeplitz matrix (also known as a diagonal-constant matrix) is a matrix in which every descending diagonal from left to right
+//  contains the same element.
+
+//Given a rectangular matrix mat, determine whether it is a Toeplitz matrix or not.
+//Implement the function isToeplitz(mat) that returns:
+
+//true if the matrix is a Toeplitz matrix
+//false otherwise
+Examples:
+
+Input: mat = [[6, 7, 8],
+                [4, 6, 7],
+                [1, 4, 6]]
+Output: true
+Explanation// The matrix is Toeplitz because every diagonal from top-left to bottom-right has the same elements. 
+//For example, the main diagonal is 6 → 6 → 6, and other diagonals like 7 → 7 and 4 → 4 are also constant. 
+//Since all diagonals follow this pattern, the matrix is Toeplitz, so the output is true.
+
+Explanation:// The matrix is Toeplitz because every diagonal from top-left to bottom-right has the same elements. 
+//For example, the main diagonal is 6 → 6 → 6, and other diagonals like 7 → 7 and 4 → 4 are also constant. 
+//Since all diagonals follow this pattern, the matrix is Toeplitz, so the output is true.
+Input: mat = [[6, 3, 8],
+                [4, 9, 7],
+                [1, 4, 6]]
+Output: false
+Explanation:// The primary diagonal elements of the given matrix are [6, 9, 6]. As the diagonal elements are not same, the given matrix
+// is not Toeplitz Matrix.
+Constraints
+//1 ≤ mat.size(), mat[0].size() ≤ 40
+//0 ≤ mat[i][j] ≤ 100
+
+
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[][]} edges
+ * @param {number} N
+ * @param {number} M
+ * @return {boolean}
+ */
+
+class Solution {
+    isToepliz(mat) {
+        
+       for(let a=1;a<mat.length;a++){
+           for(let b=1;b<mat[0].length;b++){
+               if(mat[a][b] !== mat[a-1][b-1]) return false;
+           }
+       }
+       return true;
+    }
+}

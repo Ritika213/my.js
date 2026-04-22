23a4c76bec34887e889333d341d037964d742469
Ritika213 <ritikakgupta6@gmail.com>
2026-04-22 10:55:53 +0530

    mean of range in array

---
+++ Mean of range in array.js
@@ -0,0 +1,61 @@
+//Given an integer array arr[] and a 2D array queries[][]. Each query queries[i] = [l, r] represents a subarray ranging from index 
+// l to r (inclusive). For every query, compute the mean (average) of the elements in the specified range, and return the floor value 
+// of that mean.
+
+//Return an array where each element corresponds to the result of a query.
+
+Examples:
+
+Input: arr = [1, 2, 3, 4, 5], queries = [[0, 2], [1, 3], [0, 4]]
+Output: [2, 3, 3]
+Explanation// The array is [1, 2, 3, 4, 5].
+//Query 1: l = 0, r = 2 → subarray [1, 2, 3] → sum = 6 → mean = 6/3 = 2
+//Query 2: l = 1, r = 3 → subarray [2, 3, 4] → sum = 9 → mean = 9/3 = 3
+//Query 3: l = 0, r = 4 → subarray [1, 2, 3, 4, 5] → sum = 15 → mean = 15/5 = 3
+//Hence the answer is [2, 3, 3]
+
+Input: arr = [6, 7, 8, 10], queries = [[0, 3], [1, 2]]
+Output: [7, 7]
+Explanation: //The array is [6, 7, 8, 10].
+//Query 1: l = 0, r = 3 → subarray [6, 7, 8, 10] → sum = 31 → mean = 31/4 = 7 (floor value)
+//Query 1: l = 1, r = 2 → subarray [7, 8] → sum = 15 → mean = 15/2 = 7 (floor value)
+//Hence the answer is [7, 7]
+Constraints 
+//1 ≤ arr.size() ≤ 105
+//1 ≤ arr[i] ≤ 103
+//1 ≤ queries.size() ≤ 105
+//1 ≤ queries[i][0] ≤ queries[i][1] < arr.size()
+
+/**
+ * @param {number[]} arr
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+class Solution {
+    findMean(arr, queries) {
+        let n = arr.length;
+        let pref = [];
+        let sum = 0;
+
+        for (let i = 0; i < n; i++) {
+            sum += arr[i];
+            pref.push(sum);
+        }
+
+        let ans = [];
+
+        for (let i = 0; i < queries.length; i++) {
+            let l = queries[i][0];
+            let r = queries[i][1];
+
+            let rangeSum;
+            if (l === 0) rangeSum = pref[r];
+            else rangeSum = pref[r] - pref[l - 1];
+
+            let len = r - l + 1;
+            ans.push(Math.floor(rangeSum / len)); 
+        }
+
+        return ans;
+    }
+}

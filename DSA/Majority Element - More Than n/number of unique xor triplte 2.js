829acabfd6b5df2eec6fff1df6d6a88b4a80a78c
Ritika213 <ritikakgupta6@gmail.com>
2026-07-24 13:43:36 +0530

    number of unique xor triplte

---
+++ DSA/Majority Element - More Than n/number of unique xor triplte 2.js
@@ -0,0 +1,70 @@
+//given an integer array nums.
+
+//A XOR triplet is defined as the XOR of three elements nums[i] XOR nums[j] XOR nums[k] where i <= j <= k.
+
+//Return the number of unique XOR triplet values from all possible triplets (i, j, k).
+
+ 
+
+//Example 1:
+
+Input: nums = [1,3]
+
+Output: 2
+
+Explanation
+
+//The possible XOR triplet values are:
+
+//(0, 0, 0) → 1 XOR 1 XOR 1 = 1
+//(0, 0, 1) → 1 XOR 1 XOR 3 = 3
+//(0, 1, 1) → 1 XOR 3 XOR 3 = 1
+//(1, 1, 1) → 3 XOR 3 XOR 3 = 3
+//The unique XOR values are {1, 3}. Thus, the output is 2.
+//Example 2:
+
+Input: nums = [6,7,8,9]
+
+Output: 4
+
+Explanation:
+
+//The possible XOR triplet values are {6, 7, 8, 9}. Thus, the output is 4.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 1500
+1 <= nums[i] <= 1500
+
+
+var uniqueXorTriplets = function(nums) {
+    const MAXX = 2048;
+
+    const pairXor = new Array(MAXX).fill(false);
+    const tripletXor = new Array(MAXX).fill(false);
+
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            pairXor[nums[i] ^ nums[j]] = true;
+        }
+    }
+
+    for (let x = 0; x < MAXX; x++) {
+
+        if (!pairXor[x])
+            continue;
+
+        for (const v of nums)
+            tripletXor[x ^ v] = true;
+    }
+    let ans = 0;
+
+    for (const ok of tripletXor)
+        if (ok) ans++;
+
+    return ans;
+};
